c3463e3b92be0acec06fa06479f1ba905de59677
NataliiaKolomiichenko <gella1996nr@gmail.com>
2022-10-17 21:16:43 +0300

    fix

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -14,6 +14,7 @@ const MovieDetails = () => {
     
     useEffect(() => {
         stateFrom.current = location.state?.from
+        // eslint-disable-next-line
     }, [])
 
     useEffect(() => {
